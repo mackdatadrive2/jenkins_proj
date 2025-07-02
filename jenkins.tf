fc9c262a24db3d20f336964069a2fe82be20744f
mackdatadrive2 <mackdatadrive2@gmail.com>
2025-07-02 19:31:31 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -1,8 +1,71 @@
-#Create EC2 Instance
-resource "aws_instance" "jenkins-ec2" {
-  ami                    = "ami-051f7e7f6c2f40dc1"
-  instance_type          = "t2.micro"
-  key_name        = "terraformkp"
-  tags = {
-    Name = "Myweek20project"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub") # Replace with your public key path
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_security_group"
+  description = "Allow SSH and Jenkins traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins_instance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.medium" # Or your preferred instance type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  user_data              = file("user_data.sh") # Path to user data script
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
